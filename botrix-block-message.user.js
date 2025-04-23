4bd7833536f4100b001aacd33e781622cb26f51e
NarKotiix <bryan29280@gmail.com>
2025-04-23 19:10:20 +0200

    Create botrix-block-message.user.js

---
+++ botrix-block-message.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Botrix Message Block
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bloque certains messages sur Botrix
+// @author       NarKotiix
+// @match        https://*.botrix.live/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/NarKotiix/botrix-message-block/main/botrix-message-block.user.js
+// @downloadURL  https://raw.githubusercontent.com/NarKotiix/botrix-message-block/main/botrix-message-block.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const observer = new MutationObserver(() => {
+        document.querySelectorAll('div.group.relative.px-2.lg\\:px-3').forEach(el => {
+            const contenu = el.innerText;
+            const estBotRix = contenu.includes('BotRix');
+
+            const estMessagePertePoints = /Résultat\s*:\s*\d+\s*\|.*@.*!.*perdu\s*\d+\s*points/i.test(contenu);
+            const estMessageCooldown = /@.+Vous pouvez utiliser cette commande à nouveau dans\s*\d+s/i.test(contenu);
+            const estMessageEmojisPerdus = /Le résultat est:\s*.*\|\s*@.*Désolé.*perdu\s*\d+\s*points/i.test(contenu);
+
+            if (estBotRix && (estMessagePertePoints || estMessageCooldown || estMessageEmojisPerdus)) {
+                el.remove(); // Supprimer le message de l'affichage
+            }
+        });
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+})();
